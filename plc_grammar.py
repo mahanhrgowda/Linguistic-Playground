198b136043b65c2e75605730199e63df47c81345
mahanhrgowda <mahanhrgowda@gmail.com>
2025-05-20 16:16:46 +0530

    Update plc_grammar.py

--- plc_grammar.py
+++ plc_grammar.py
@@ -1,13 +1,10 @@
 import nltk
-from nltk import pos_tag, word_tokenize
 
-nltk.download('punkt')
-# Ensure necessary NLTK resources are downloaded
+# Ensure NLTK tokenizer resources are available
 nltk.download("punkt", quiet=True)
-nltk.download('averaged_perceptron_tagger')
-# Ensure necessary NLTK resources are downloaded
 nltk.download("averaged_perceptron_tagger", quiet=True)
 
+from nltk import word_tokenize, pos_tag
 
 def analyze_grammar(sentence):
     tokens = word_tokenize(sentence)
